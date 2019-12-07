3d0cc691b0993b1b323e2dc8d8ba203a7b251ef9
mdip226 <51466371+mdip226@users.noreply.github.com>
2019-12-07 16:33:41 -0500

    sped up some more

--- followtheline.py
+++ followtheline.py
@@ -8,7 +8,7 @@ def cozmo_program(robot: cozmo.robot.Robot):
     time.sleep(1)
     robot.set_lift_height(cozmo.robot.MAX_LIFT_HEIGHT_MM).wait_for_completed()
     robot.set_head_angle(cozmo.robot.MIN_HEAD_ANGLE).wait_for_completed()
-    for i in range(100):
+    for i in range(1000):
         
         
         image = robot.world.latest_image.raw_image
@@ -18,7 +18,7 @@ def cozmo_program(robot: cozmo.robot.Robot):
         image = enhancer.enhance(10.0)
         image = ImageOps.invert(image)
         image = image.convert(mode="1")
-        image.save("%s.jpg" % i)
+        #image.save("%s.jpg" % i)
         left_side = image.crop((0,0,(image.width/2),image.height))
         right_side = image.crop(((image.width/2),0,image.width,image.height))
         #left_side.save("left%s.jpg" % i)
@@ -35,47 +35,47 @@ def cozmo_program(robot: cozmo.robot.Robot):
         if diff < -2000:
             print("hard left")
             robot.turn_in_place(degrees(20)).wait_for_completed()
-            robot.drive_straight(distance_mm(10), speed_mmps(50), should_play_anim=False).wait_for_completed()
+            robot.drive_straight(distance_mm(15), speed_mmps(100), should_play_anim=False).wait_for_completed()
             #robot.drive_wheels(-20,10)
             
         if diff > 2000:
             print("hard right")
             robot.turn_in_place(degrees(-20)).wait_for_completed()
-            robot.drive_straight(distance_mm(10), speed_mmps(50), should_play_anim=False).wait_for_completed()
+            robot.drive_straight(distance_mm(15), speed_mmps(100), should_play_anim=False).wait_for_completed()
             
             #robot.drive_wheels(10,-20)
         if diff < -1000 and diff >= -2000:
             print("turning left")
             robot.turn_in_place(degrees(10)).wait_for_completed()
-            robot.drive_straight(distance_mm(10), speed_mmps(50), should_play_anim=False).wait_for_completed()
+            robot.drive_straight(distance_mm(15), speed_mmps(100), should_play_anim=False).wait_for_completed()
             #robot.drive_wheels(0,20)
             
         if diff > 1000 and diff <= 2000:
             print("turning right")
             robot.turn_in_place(degrees(-10)).wait_for_completed()
-            robot.drive_straight(distance_mm(10), speed_mmps(50), should_play_anim=False).wait_for_completed()
+            robot.drive_straight(distance_mm(15), speed_mmps(100), should_play_anim=False).wait_for_completed()
             #robot.drive_wheels(20,0)
         if diff < -500 and diff >= -1000:
             print("slight left")
             robot.turn_in_place(degrees(5)).wait_for_completed()
-            robot.drive_straight(distance_mm(10), speed_mmps(50), should_play_anim=False).wait_for_completed()
+            robot.drive_straight(distance_mm(15), speed_mmps(100), should_play_anim=False).wait_for_completed()
             #robot.drive_wheels(0,10)
         if diff > 500 and diff <= 1000:
             print("slight right")
             robot.turn_in_place(degrees(-5)).wait_for_completed()
-            robot.drive_straight(distance_mm(10), speed_mmps(50), should_play_anim=False).wait_for_completed()
+            robot.drive_straight(distance_mm(15), speed_mmps(100), should_play_anim=False).wait_for_completed()
             #robot.drive_wheels(10,0)
             
         if diff > -500 and diff < 500:
             print("driving straight")
-            robot.drive_straight(distance_mm(20), speed_mmps(50), should_play_anim=False).wait_for_completed()
+            robot.drive_straight(distance_mm(30), speed_mmps(100), should_play_anim=False).wait_for_completed()
             #robot.drive_wheels(10,10)
         robot.set_head_angle(cozmo.robot.MIN_HEAD_ANGLE).wait_for_completed()
         #robot.drive_straight(distance_mm(50), speed_mmps(50), should_play_anim=False).wait_for_completed()
         #robot.turn_in_place(degrees(90)).wait_for_completed()
         
         
-        time.sleep(1)
+        #time.sleep(1)
     
     
 cozmo.run_program(cozmo_program, use_viewer = False)
